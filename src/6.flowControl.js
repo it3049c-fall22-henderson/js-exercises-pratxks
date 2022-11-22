2a6d930135034ac63eb12b4bc44d63675dc3d294
pratxks <101081557+pratxks@users.noreply.github.com>
2022-11-22 16:29:32 +0530

    finished test flowControl

--- src/6.flowControl.js
+++ src/6.flowControl.js
@@ -7,10 +7,14 @@
  *    if no number was provided or if the value provided wasn't a number (hint: typeof), return false
  */
 function fizzBuzz(num) {
-  if ((num % 3 == 0) && (num % 5 == 0)) return 'fizzbuzz';
-  else if (num % 3 == 0) return 'fizz';
-  else if (num % 5 == 0) return 'buzz';
-  else return num;
+  if (typeof num == 'number') {
+    if ((num % 3 == 0) && (num % 5 == 0)) return 'fizzbuzz';
+    else if (num % 3 == 0) return 'fizz';
+    else if (num % 5 == 0) return 'buzz';
+    else return num;
+  }
+
+  return false;
 }
 
 module.exports = {

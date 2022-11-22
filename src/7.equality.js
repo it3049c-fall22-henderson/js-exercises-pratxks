b35e61e863efca562da7f5a06b9c32cdaf2bb47e
pratxks <101081557+pratxks@users.noreply.github.com>
2022-11-22 16:35:08 +0530

    finished testing: equality

--- src/7.equality.js
+++ src/7.equality.js
@@ -2,14 +2,18 @@
 // Example: strictEquality(1, true) => false
 // Example: strictEquality(1, 1) => true
 function strictEquality(val1, val2) {
-  return ;
+  if ((typeof val1 == typeof val2) && (val1 == val2)) return true;
+
+  return false;
 }
 //equality: compare value but not necessarily the type
 // Example: equality(1, true) => true
 // Example: equality(1, 1) => true
 // Example: equality(1, false) => false
 function equality(val1, val2) {
-  return ;
+  if (val1 == val2) return true;
+
+  return false;
 }
 
 module.exports = {
